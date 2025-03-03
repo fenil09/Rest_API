23da8116faf3d380928a3c703bc0cd756282588b
FenilAhir98980 <backupsystem9909@gmail.com>
2025-03-03 04:43:54 -0800

    Integrated MongoDb with the REST API to perform CRUD operations.

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const app= express();
 const fs =require('fs');
 const users= require("./MOCK_DATA.json");
 const { error } = require('console');
+
+// requiring the mongoose package helping us to connect to mongodb
+const mongoose = require("mongoose");
+const { type } = require('os');
+const { json } = require('stream/consumers');
+
+
+
+// setting up the connection with mongodb, just use the mongoose object and pass the url on which
+// the mongodb is running.
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/backend').then(() => console.log("connection done"));
+
+// creating a schema
+
+const userschema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required: true,
+    },
+    lastname:{ 
+        type:String
+    },
+    gender:{
+        type:String
+    }
+})
+
+// creating a model now helping to interact with mongodb
+const usermodel = mongoose.model("user",userschema);
+
+
+
+
+
+
 // middleware to parse the json data.
 app.use(express.urlencoded({extended:false}));
 
@@ -18,20 +55,21 @@ app.use(express.urlencoded({extended:false}));
     
  })
 // Getting all the users
-app.get("/users",function(request,response){
+app.get("/users",async function (request,response){
     response.setHeader("myheader","Fenil")
     // adding X before the header name can be seen as the best practice.
-
-    response.json(users);
+    const alldatabaseuser =  await usermodel.find({});
+    response.json(alldatabaseuser);
+    
 })
 
 // Getting a particular user.
 
-app.get("/users/:id",function(request,response){
-    const id = Number(request.params.id);
+app.get("/users/:id",async function(request,response){
+    const id = request.params.id;
     // getting the ID so that we can match use it for performing the matching logic
-    const user = users.find(user => user.id === id);
-    response.send(user);
+    const founduser = await usermodel.findById(id);
+    response.json(founduser);
 })
 
 // Creating a new user.
@@ -40,45 +78,34 @@ app.post("/users",function(request,response){
      const body = request.body;
      // Adding a check which would see if the user is sending all the fields with the request or not
      // if not we can handle it by using the 400 status code which stands for the bad request.
-     if(!body.first_name || !body.last_name || !body.email || !body.gender){
+     if(!body.first_name || !body.last_name ||  !body.gender){
        return response.status(400).send("Please provide all the fields")
      }
-    users.push({
-        id : users.length+1,
-        first_name : body.first_name,
-        last_name : body.last_name,
-        email : body.email ,
-        gender : body.gender,
-    })
-    // converting the JSON into string data.
-    const userwritable = JSON.stringify(users);
-       // adding the new data back to the json file.
-    fs.writeFile("./MOCK_DATA.json",userwritable,function(err){
-        if(err) console.log(err.message);
-        else console.log("Writing finished");
-    })
- 
-    response.status(201).send("New data added");
+      usermodel.create({
+        firstname : body.first_name,
+        lastname : body.last_name,
+        gender: body.gender
+      })
+      response.status(201).send("User created");
+
     // changing the status code to 201, indicating creation of a successfull entity.
 })
 // Removing a user
 
- app.delete("/users/:id",function(request,response){
-    const id = Number(request.params.id)
-    const index = users.findIndex(user => user.id === id);
-    users.splice(index);
-    response.send("User Removed successfully");
+ app.delete("/users/:id",async function(request,response){
+    const id = request.params.id
+       await usermodel.findByIdAndDelete(id);
+       response.json({status : "Success"});
  })
 
  // Updating the specific user.
  
- app.patch("/users/:id",function(request,response){
-    const id = Number(request.params.id);
-    const user = users.find(user => user.id === id);
-     Object.assign(user, request.body);
-     response.send("User Updated Successfully");
+ app.patch("/users/:id",async function(request,response){
+    const id = request.params.id;
+    const body = request.body;
+   await usermodel.findByIdAndUpdate(id, {gender: body.gender});
+    response.json({status : "success"});
  })
-
 
 const port = 8000;
 
